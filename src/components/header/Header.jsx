3772c9d3a74e6e2f2d910bdaa3206c9ced8c2ecf
Belinda Shan <belinda126@live.cn>
2022-08-29 16:00:33 +1000

    nav bar content finished

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./header.css";
+import CTA from "./CTA";
+import HeaderSocials from "./HeaderSocials";
+
+function Header() {
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello, I'm</h5>
+        <h1>Belinda Shan</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+        <HeaderSocials />
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
