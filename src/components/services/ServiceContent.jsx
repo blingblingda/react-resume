432e78510b7146771e783fb97ab623930e4b4f29
Belinda Shan <belinda126@live.cn>
2022-09-07 18:06:51 +1000

    wrap service icon with div to fix the icon size

--- src/components/services/ServiceContent.jsx
+++ src/components/services/ServiceContent.jsx
@@ -5,7 +5,7 @@ import { BiCheck } from "react-icons/bi";
 const ServiceContent = (props) => {
   return (
     <li>
-      <BiCheck className="service__list-icon" />
+      <div className="service__list-icon"><BiCheck /></div>
       <p>{props.content}</p>
     </li>
   );
