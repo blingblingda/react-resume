130e856c7b94041d332ecb60437fab98669fa315
Belinda Shan <belinda126@live.cn>
2022-10-17 16:13:10 +1100

    update HeaderIntro component

--- src/components/header/HeaderIntro.jsx
+++ src/components/header/HeaderIntro.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
 
 const HeaderIntro = () => {
   return (
     <section id="intro">
-      <p class="name">
-        Hello, my name is <span>Belinda Shan</span>
-      </p>
-      <h2>I build things for the web.</h2>
-      <p>I'm an accountant turned and self-taught front-end developer.</p>
-      <p>Now I'm focusing on building my skills in front-end development.</p>
+      <Container className="container-lg mt-5">
+        <h1 className="pt-5">
+          <div className="fs-6 text-muted mb-3">Hello, my name is</div>
+          <div className="display-3 fw-bold">Belinda Shan</div>
+        </h1>
+        <h2 className="display-4 mt-4 fw-bold text-muted">I build things for the web.</h2>
+        <p className="fs-6 mt-5">I'm an accountant turned and self-taught front-end developer.</p>
+        <p className="fs-6">Now I'm focusing on building my skills in front-end development.</p>
+      </Container>
     </section>
   );
 };
